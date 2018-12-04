a68590cc8668b0d7c6ce05f3e2de6c2d6249fd4d
klyjm <519812260@qq.com>
2018-12-04 20:28:54 +0800

    LeNet-5跑了一次

--- CNN.py
+++ CNN.py
@@ -22,18 +22,18 @@ def train(filename):
     y = tf.placeholder(tf.float32, [None, 10])
     inputdata = tf.reshape(x, [-1, 28, 28, 1])
     conv1w = tf.Variable(tf.truncated_normal([5, 5, 1, 32], stddev=0.1))
-    conv1b = tf.Variable(tf.constant(0.0, shape=[32]))
-    conv1 = tf.nn.conv2d(x, conv1w, strides=[1, 1, 1, 1], padding='SAME')
+    conv1b = tf.Variable(tf.constant(0.1, shape=[32]))
+    conv1 = tf.nn.conv2d(inputdata, conv1w, strides=[1, 1, 1, 1], padding='SAME')
     relu1 = tf.nn.relu(tf.nn.bias_add(conv1, conv1b))
     pool1 = tf.nn.max_pool(relu1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-    conv2w = tf.Variable(tf.truncated_normal([5, 5, 1, 64], stddev=0.1))
-    conv2b = tf.Variable(tf.constant(0.0, shape=[64]))
+    conv2w = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev=0.1))
+    conv2b = tf.Variable(tf.constant(0.1, shape=[64]))
     conv2 = tf.nn.conv2d(pool1, conv2w, strides=[1, 1, 1, 1], padding='SAME')
     relu2 = tf.nn.relu(tf.nn.bias_add(conv2, conv2b))
     pool2 = tf.nn.max_pool(relu2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     poolshape = pool2.get_shape().as_list()
     nodes = poolshape[1] * poolshape[2] * poolshape[3]
-    reshaped = tf.reshape(pool2, [poolshape[0], nodes])
+    reshaped = tf.reshape(pool2, [-1, nodes])
     fc1w = tf.Variable(tf.truncated_normal([nodes, 512], stddev=0.1))
     tf.add_to_collection('loss', tf.contrib.layers.l2_regularizer(0.0001)(fc1w))
     fc1b = tf.Variable(tf.constant(0.1, shape=[512]))
@@ -44,6 +44,26 @@ def train(filename):
     fc2b = tf.Variable(tf.constant(0.1, shape=[10]))
     lenet5y = tf.matmul(fc1, fc2w) + fc2b
     globalstep = tf.Variable(0, trainable=False)
+    varave = tf.train.ExponentialMovingAverage(0.99, globalstep)
+    varaveop = varave.apply(tf.trainable_variables())
+    crossentropy = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=lenet5y)
+    crossentrogymean = tf.reduce_mean(crossentropy)
+    loss = crossentrogymean + tf.add_n(tf.get_collection('loss'))
+    learnrate = tf.train.exponential_decay(0.8, globalstep, 400, 0.99)
+    trainstep = tf.train.GradientDescentOptimizer(learnrate).minimize(loss, global_step=globalstep)
+    trainop = tf.group(trainstep, varaveop)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        ckpt = tf.train.get_checkpoint_state('/ckpt/')
+        if ckpt and ckpt.model_checkpoint_path:
+            saver.restore(sess, ckpt.model_checkpoint_path)
+        for i in range(20):
+            for j in range(400):
+                x1 = imagedata[j * batchsize:(j + 1) * batchsize]
+                y1 = onehotlabel[j * batchsize:(j + 1) * batchsize]
+                sess.run([trainop, loss, globalstep], feed_dict={x: x1, y: y1})
+        saver.save(sess, '/ckpt/lenet5.ckpt')
 
 
 def datatest(filename):
